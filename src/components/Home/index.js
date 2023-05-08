f12c6b4f42cd05de6f139663b36b791ccb6b8022
Tarun Suresh <tarunsuresh.v@gmail.com>
2023-05-08 22:58:42 +0530

    Complete Home Page

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,1 +1,54 @@
-// Write your code here
+import './index.css'
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+import TeamCard from '../TeamCard'
+
+class Home extends Component {
+  state = {teamCardsList: [], isLoading: true}
+
+  componentDidMount() {
+    this.getTeamCardsListData()
+  }
+
+  getTeamCardsListData = async () => {
+    const response = await fetch('https://apis.ccbp.in/ipl')
+    const data = response.json()
+    const {teams} = data
+    const FilteredData = teams.map(eachTeam => ({
+      id: eachTeam.id,
+      name: eachTeam.name,
+      teamImageUrl: eachTeam.team_image_url,
+    }))
+
+    this.setState({teamCardsList: FilteredData, isLoading: false})
+  }
+
+  render() {
+    const {isLoading, teamCardsList} = this.state
+    return (
+      <div className="home-container">
+        {isLoading ? (
+          <Loader type="Oval" color="#ffffff" height={50} width={50} />
+        ) : (
+          <>
+            <div className="home-title-container">
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/ipl-logo-img.png"
+                className="ipl-logo"
+                alt="ipl logo"
+              />
+              <h1 className="home-title">IPL Dashboard</h1>
+            </div>
+            <div className="team-cards-container">
+              {teamCardsList.map(eachCard => (
+                <TeamCard key={eachCard.id} cardDetail={eachCard} />
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+    )
+  }
+}
+
+export default Home
--- src/components/TeamCard/index.js
+++ src/components/TeamCard/index.js
@@ -1,1 +1,16 @@
-// Write your code here
+import './index.css'
+import {Link} from 'react-router-dom'
+
+const TeamCard = props => {
+  const {cardDetail} = props
+
+  const {id, name, teamImageUrl} = cardDetail
+  return (
+    <Link className="team-card" to={`/team-matches/${id}`}>
+      <img src={teamImageUrl} className="team-card-img" alt={name} />
+      <h1 className="team-card-name">{name}</h1>
+    </Link>
+  )
+}
+
+export default TeamCard
