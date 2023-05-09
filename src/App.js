01315ee842b430e8adf41bbd4f44bc293f642d0b
tommy <tarunsuresh.v@gmail.com>
2023-05-09 10:54:40 +0000

    Add Team Matches & Not Found Route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import Home from './components/Home'
+import TeamMatches from './components/TeamMatches'
+import NotFound from './components/NotFound'
 
 const App = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/team-matches/:id" component={TeamMatches} />
+      <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
 )
