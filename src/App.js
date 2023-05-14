eda76e0ecb1e500b3378387de6008c10e226a2f5
Tarun suresh <88505951+tarunsuresh06@users.noreply.github.com>
2023-05-14 18:56:55 +0530

    Fixed bugs App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import TeamMatches from './components/TeamMatches'
 import NotFound from './components/NotFound'
 
 const App = () => (
-  <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/team-matches/:id" component={TeamMatches} />
       <Route component={NotFound} />
     </Switch>
-  </BrowserRouter>
 )
 
 export default App
